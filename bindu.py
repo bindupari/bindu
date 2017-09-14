bf6d02969268db82900521f289502402e3f1b6bd
bindupari <31947714+bindupari@users.noreply.github.com>
2017-09-14 10:57:01 +0530

    Add files via upload

---
+++ bindu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+a=23
+print(a*4)
+print(a+3)
+print(a-5)
+print(a/3)
+print(cmr)
+print(cmr)is a big university
+print(name*4)
+print (nmae cmr*3
+	)
---
+++ bindudice.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#guess what this program does???
+import random
+r=random.randint(1,6)#gives random randint
+print (r)
+if r==6:
+   print("congrats u got 6")
