72d08dd1e1bb6361024d0cb9169d3e340e591c24
bindupari <31947714+bindupari@users.noreply.github.com>
2017-09-28 03:53:20 +0000

    Update bindu.py

--- bindu.py
+++ bindu.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 a=23
 print(a*4)
+#it multiples 4823 and gives 92
 print(a+3)
+#it adds 23+3 and gives 26
 print(a-5)
+#it substracts 23-5 and gives 18
 print(a/3)
+#it divides 23/3 and gives 7.6
 print(cmr)
+#it prints as cmr
 print(cmr)is a big university
+#it prints as cmr is a big university
 print(name*4)
-print (nmae cmr*3
-	)
+#it prints cmr 4 times
+print (nmae cmr*3)
