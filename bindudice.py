97e1f9990c89883207d18a64f5c16cb0ea39ca76
bindupari <31947714+bindupari@users.noreply.github.com>
2017-09-28 03:55:58 +0000

    Update bindudice.py

--- bindudice.py
+++ bindudice.py
@@ -2,6 +2,8 @@
 #guess what this program does???
 import random
 r=random.randint(1,6)#gives random randint
+#it gives the random number from 1 to 6
 print (r)
 if r==6:
    print("congrats u got 6")
+#when dice rolled congrats u got 6
